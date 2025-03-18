3a89fbf046b07f7b18c0a4519cb4834618a415c5
Erick <erick2ribeirogg@gmail.com>
2025-03-18 15:10:57 -0300

    Delete Exercícios/Semana5/Ex1.java

--- Exercícios/Semana5/Ex1.java
+++
@@ -1,9 +0,0 @@
-/* 1: Verificação de Número Par ou Ímpar 
-Descrição: 
-Crie um programa que solicite um número inteiro ao usuário e determine se ele é  par ou ímpar, utilizando a estrutura if-else. 
-Requisitos: 
-• O programa deve ler um número inteiro. 
-• Se o número for divisível por 2, exibir "O número é par." 
-• Caso contrário, exibir "O número é ímpar.".  */
-
-
