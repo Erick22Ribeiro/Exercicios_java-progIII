5e53a16bd8648304d788c0fe2caffd3e5548a148
Erick <erick2ribeirogg@gmail.com>
2025-03-20 13:49:08 -0300

    Update Ex9.java

--- Exercícios/Semana3/Ex9.java
+++ Exercícios/Semana3/Ex9.java
@@ -7,17 +7,14 @@ public class Ex9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Solicita o valor em reais e a cotacao do dolar
         System.out.print("Digite o valor em reais: R$ ");
         double reais = scanner.nextDouble();
 
         System.out.print("Digite a cotacao do dolar: R$ ");
         double cotacaoDolar = scanner.nextDouble();
 
-        // Calcula o valor em dolares
         double dolares = reais / cotacaoDolar;
 
-        // Exibe o resultado
         System.out.printf("O valor em dolares e: US$ %.2f\n", dolares);
 
         scanner.close();
