b79e612b09712e09691151202b80b6a3a2141b34
Erick <erick2ribeirogg@gmail.com>
2025-03-20 13:48:36 -0300

    Update Ex7.java

--- Exercícios/Semana3/Ex7.java
+++ Exercícios/Semana3/Ex7.java
@@ -14,18 +14,15 @@ public class Ex7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Solicita a base e altura do retangulo
         System.out.print("Digite a base do retangulo: ");
         double base = scanner.nextDouble();
         
         System.out.print("Digite a altura do retangulo: ");
         double altura = scanner.nextDouble();
 
-        // Calcula a area e o perimetro
         double area = base * altura;
         double perimetro = 2 * (base + altura);
-
-        // Exibe o resultado
+        
         System.out.printf("A area do retangulo e: %.2f\n", area);
         System.out.printf("O perimetro do retangulo e: %.2f\n", perimetro);
 
