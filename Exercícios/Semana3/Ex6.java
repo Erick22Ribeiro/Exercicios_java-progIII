268f148090f1fabbb486615a88acb08f85ad955b
Erick <erick2ribeirogg@gmail.com>
2025-03-20 13:48:23 -0300

    Update Ex6.java

--- Exercícios/Semana3/Ex6.java
+++ Exercícios/Semana3/Ex6.java
@@ -7,17 +7,14 @@ public class Ex6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Solicita o salario bruto e o desconto do INSS
         System.out.print("Digite o salario bruto: R$ ");
         double salarioBruto = scanner.nextDouble();
         
         System.out.print("Digite o valor do desconto do INSS: R$ ");
         double descontoINSS = scanner.nextDouble();
-
-        // Calcula o salario liquido
+        
         double salarioLiquido = salarioBruto - descontoINSS;
 
-        // Exibe o resultado
         System.out.printf("O salario liquido e: R$ %.2f\n", salarioLiquido);
 
         scanner.close();
