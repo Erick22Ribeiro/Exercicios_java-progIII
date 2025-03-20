481acedf255ab8e472eda304bd9a26d26dcfd16f
Erick <erick2ribeirogg@gmail.com>
2025-03-20 13:48:51 -0300

    Update Ex8.java

--- Exercícios/Semana3/Ex8.java
+++ Exercícios/Semana3/Ex8.java
@@ -10,15 +10,12 @@ import java.util.Scanner;
 public class Ex8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Solicita o raio da esfera
+        
         System.out.print("Digite o raio da esfera: ");
         double raio = scanner.nextDouble();
 
-        // Calcula o volume da esfera
         double volume = (4.0 / 3.0) * Math.PI * Math.pow(raio, 3);
 
-        // Exibe o resultado
         System.out.printf("O volume da esfera e: %.2f\n", volume);
 
         scanner.close();
