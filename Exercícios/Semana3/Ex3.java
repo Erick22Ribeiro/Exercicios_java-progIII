7560378de226c34c068d921d10f3a4759cc14881
Erick <erick2ribeirogg@gmail.com>
2025-03-20 13:47:15 -0300

    Update Ex3.java

--- Exercícios/Semana3/Ex3.java
+++ Exercícios/Semana3/Ex3.java
@@ -7,20 +7,16 @@ import java.util.Scanner;
 
 public class Ex3 {
     public static void main(String[] args) {
-        // Criação do objeto Scanner para ler a entrada do usuário
+ 
         Scanner scanner = new Scanner(System.in);
 
-        // Solicita ao usuário que insira a temperatura em Celsius
         System.out.print("Digite a temperatura em graus Celsius: ");
         double celsius = scanner.nextDouble();
 
-        // Converte a temperatura de Celsius para Fahrenheit
         double fahrenheit = (celsius * 9 / 5) + 32;
 
-        // Exibe o resultado
         System.out.printf("A temperatura em Fahrenheit e: %.2f\n", fahrenheit);
 
-        // Fecha o scanner
         scanner.close();
     }
 }
